eba3cfbd0e7ac16812cc65c99f9625f6d9d3f5f8
Alexandre Plennevaux <alexandre@pixeline.be>
2019-02-20 00:00:04 +0100

    working version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # portable-github-issues-form
+
+## how to use 
+
+
+## Development 
+
+`npm install -g watchify browserify`
+
+then
+
+`watchify portable-github-issues-form.js -o bundle.js` 
--- portable-github-issues-form.js
+++ portable-github-issues-form.js
@@ -1,23 +1,70 @@
-var createIssue = require('github-create-issue');
 
-var opts = {
-    'token': '304aa57fd077ca9562376f3dea91fe3091245afe',
-    'useragent': 'beep-boop-bop',
-    'body': 'Beep boop.'
-};
-
-createIssue('pixeline/portable-github-issues-form', 'Big bug.', opts, clbk);
-
-function clbk(error, issue, info) {
-    // Check for rate limit information...
-    if (info) {
-        console.error('Limit: %d', info.limit);
-        console.error('Remaining: %d', info.remaining);
-        console.error('Reset: %s', (new Date(info.reset * 1000)).toISOString());
+function Issue(options) {
+    this.token = options.token || '1b508508fcf3c7ec30dfa3aba9697bc51c3896fd';
+    this.useragent = options.useragent || 'portable-github-issues-form';
+    this.repository = options.repository || 'pixeline/portable-github-issues-form';
+    this.DOMElement = null;
+    var issue = this;
+    if (!issue.token || !issue.repository) {
+        throw new Error('Cannot post to Github : No token or repository provided.');
     }
-    if (error) {
-        throw new Error(error.message);
+    function send(title, content) {
+        var createGithubIssue = require('github-create-issue');
+        // Basic validation:
+        if (!title || !content) {
+            var feedback = 'Cannot post to Github : No Title or Content filled.';
+            alert(feedback);
+            throw new Error(feedback);
+        }
+        // Actually post the Issue:
+        createGithubIssue(issue.repository, title, {
+            'token': issue.token,
+            'useragent': issue.useragent,
+            'body': content
+        }, githubCallback);
     }
-    console.log(JSON.stringify(issue));
-    // returns <issue_data>
+
+    function githubCallback(error, issue, info) {
+        // Check for rate limit information...
+        if (info) {
+            console.error('Limit: %d', info.limit);
+            console.error('Remaining: %d', info.remaining);
+            console.error('Reset: %s', (new Date(info.reset * 1000)).toISOString());
+        }
+        if (error) {
+            throw new Error(error.message);
+        }
+        alert("Message sent. Thank you!");
+        document.getElementById("js-github-form").reset();
+
+        //console.log(JSON.stringify(issue));
+        // returns <issue_data>
+    }
+    this.form = '<div id="js-github-issue-form" class="portable-github-form"><span id="js-github-issue-form-close" class="github-issue-form-close"></span><form id="js-github-form"><fieldset><legend>Report an issue</legend><div><label for="git-title">Title</label><input tabindex="0" type="text" id="git-title" name="title"></div><div><label for="git-content">What is the problem ?</label><textarea tabindex="1" id="git-content" name="content" placeholder="Please describe steps to reproduce the issue..."></textarea></div><div><input tabindex="2" type="submit" name="send" id="js-github-issues-form-send" class="github-issues-form-send" value="Send this issue"></div></fieldset></form></div>';
+
+    this.inject = function () {
+        var el = document.querySelector('body');
+        el.insertAdjacentHTML("beforeend", this.form);
+        el = document.getElementById('js-github-issues-form');
+        // bind Submit event
+        var submit = document.getElementById('js-github-form');
+        submit.addEventListener("submit", function (e) {
+            e.preventDefault();
+            var title = document.getElementById('git-title').value;
+            var body = document.getElementById('git-content').value;
+            send(title, body);
+        });
+        // bind close button event
+        var close = document.getElementById('js-github-issue-form-close');
+        close.addEventListener("click", function () {
+            // Remove the form 
+            el = document.getElementById('js-github-issue-form');
+            el.remove();
+        });
+    }
 }
+
+// RUNTIME
+Issue = new Issue({ 'token': '1b508508fcf3c7ec30dfa3aba9697bc51c3896fd', 'useragent': 'portable-github-issues-form', 'repository': 'pixeline/portable-github-issues-form' });
+Issue.inject();
+//Issue.send('yolo', 'This is just a test man! Chill!');
